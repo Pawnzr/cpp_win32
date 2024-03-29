cf995a62a6b0870cdbda10abe106de75790ac9d5
Pawn3r <anduu150620@gmail.com>
2024-03-29 09:29:57 +0700

    SetTimer function

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+#include <Windows.h>
+
+/*
+Hàm SetTimer trong th? vi?n winuser.h ???c s? d?ng ?? t?o m?t b? h?n gi? (timer) và thi?t l?p m?t s? ki?n ?? g?i l?i (callback) sau m?t kho?ng th?i gian nh?t ??nh
+UINT_PTR SetTimer(
+  HWND      hWnd,           // C?a s? mà hàm g?i l?i s? ???c g?i t?i
+  UINT_PTR  nIDEvent,       // ID c?a b? h?n gi?
+  UINT      uElapse,        // Th?i gian gi?a các l?n g?i l?i (miligiây)
+  TIMERPROC lpTimerFunc     // Con tr? ??n hàm g?i l?i
+);
+*/
+
+int main() {
+	UNIT_PTR timerID = SetTimer()
+}
